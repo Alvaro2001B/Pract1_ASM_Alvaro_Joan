8a4e3862b607be35ab5ca9c2374839c344cb02e4
Alvaro <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-01 11:31:45 +0200

    SplashScreen

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     //Prueba
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
