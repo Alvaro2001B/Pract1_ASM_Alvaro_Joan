42396593b71c2862113fb7befe0c98e133029aec
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-02 17:23:18 +0200

    prueba cambiar pantalla

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
@@ -6,11 +6,16 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     //Prueba
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Thread.sleep(5000)
         setTheme(R.style.Theme_Pract1_ASM_Alvaro_Joan)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inicio_session_button = findViewById<Button>(R.id.inicio_session)
+        inicio_session_button.setOnClickListener{
+            val intent = Intent(this,MainActivity2 startActivity(intent))}
+        }
     }
 }
