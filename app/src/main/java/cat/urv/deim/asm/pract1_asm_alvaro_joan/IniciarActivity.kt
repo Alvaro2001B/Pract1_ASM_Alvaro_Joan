ace5d54e82ab583775f0fbbcdf71c2223a8dc6af
JoanTorresFlores <joantorres2001@gmail.com>
2022-04-02 18:56:40 +0200

    Cambio de pantallas (final)

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
@@ -3,9 +3,11 @@ package cat.urv.deim.asm.pract1_asm_alvaro_joan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class IniciarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
     }
 }
--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/MainActivity.kt
@@ -3,6 +3,7 @@ package cat.urv.deim.asm.pract1_asm_alvaro_joan
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     //Prueba
@@ -12,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val signUpButton: Button=findViewById<Button>(R.id.Registrar_se)
+        signUpButton.setOnClickListener {
+            val intent: Intent = Intent()
+            intent.setClass(this, RegistreActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        val inicio_session_button= findViewById<Button>(R.id.inicio_session)
+            inicio_session_button.setOnClickListener{
+                val intent2: Intent = Intent()
+                intent2.setClass(this, IniciarActivity::class.java)
+                this.startActivity(intent2)
+            }
+        }
+
     }
-}
