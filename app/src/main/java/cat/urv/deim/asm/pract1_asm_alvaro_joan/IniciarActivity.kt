bcf54ca27691a31e1a9369c44b57ad7292caeaa7
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-03 20:08:15 +0200

    Patinetes

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
@@ -1,13 +1,20 @@
 package cat.urv.deim.asm.pract1_asm_alvaro_joan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IniciarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar)
 
-
+        val inicio_session_button= findViewById<Button>(R.id.button_iniciar)
+        inicio_session_button.setOnClickListener{
+            val intent2: Intent = Intent()
+            intent2.setClass(this, PatinetesActivity::class.java)
+            this.startActivity(intent2)
+        }
     }
 }
