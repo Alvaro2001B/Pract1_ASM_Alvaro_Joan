0f3291e09503f321c2c59aa725fa8743eab4e097
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-03 18:51:12 +0200

    Pagina de iniciar sesion

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/IniciarActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class IniciarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_iniciar)
 
 
     }
