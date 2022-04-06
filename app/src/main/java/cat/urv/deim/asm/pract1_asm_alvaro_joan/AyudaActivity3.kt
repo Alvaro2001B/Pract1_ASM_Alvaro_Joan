e69ca3a8aaf5204dd939529fe22b6f17a86b4a35
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-06 18:11:37 +0200

    onboarding

---
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/AyudaActivity3.kt
@@ -0,0 +1,11 @@
+package cat.urv.deim.asm.pract1_asm_alvaro_joan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AyudaActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayuda3)
+    }
+}
--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/Board.kt
+++
@@ -1,9 +0,0 @@
-package cat.urv.deim.asm.pract1_asm_alvaro_joan
-
-data class Board(
-    val fondo:Int,
-    val imagen: Int,
-    val titulo: String,
-    val descripcion: String
-
-)
