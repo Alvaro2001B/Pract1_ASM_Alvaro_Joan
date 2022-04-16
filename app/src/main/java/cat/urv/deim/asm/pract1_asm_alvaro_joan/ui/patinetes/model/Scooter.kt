5520a81f6743a9c9e2da5f87b46f7fb0060a37f5
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-04-16 19:53:31 +0200

    Recycle

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/model/Scooter.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/model/Scooter.kt
@@ -1,5 +1,9 @@
 package cat.urv.deim.asm.pract1_asm_alvaro_joan.ui.patinetes.model
 
-class Scooter(val name: String, val uuid: String) {
+import android.provider.ContactsContract
+import java.time.Year
+import java.util.*
 
-}
+class Scooter(val name: String, val uuid: String,val longitud: Double,val latitud: Double,val nivel_bateria:Int,val km_recorreguts : Double,val año:Int,val mes: Int,val dia:Int, val estat :String) {
+    val data_manteniment = Date(año, mes, dia)
+ }
--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/repositories/ScooterRepository.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/repositories/ScooterRepository.kt
@@ -34,7 +34,7 @@ class ScooterRepository {
             val uuidList: Array<String> =AppConfig.DEFAULT_SCOOTERS_ID_ARRAY
             var scooter: Scooter
             uuidList.forEach {
-                scooter = Scooter(uuid = it, name = it)
+                scooter = Scooter(uuid = it, name = it, longitud = it.toDouble(), latitud = it.toDouble(), nivel_bateria = it.toInt(), km_recorreguts = it.toDouble(), año = it.toInt(), mes = it.toInt(), dia = it.toInt(),estat=it)
                 scooters.scooters.add(scooter)
             }
 
