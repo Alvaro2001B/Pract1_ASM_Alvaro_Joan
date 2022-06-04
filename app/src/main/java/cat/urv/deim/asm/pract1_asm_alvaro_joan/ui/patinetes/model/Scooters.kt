e4f866d118e0188261d2c189ce556e2ebbb6414c
Alvaro <100117640+Alvaro2001B@users.noreply.github.com>
2022-06-04 11:58:36 +0200

    arreglos

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/model/Scooters.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/model/Scooters.kt
@@ -1,8 +1,9 @@
 package cat.urv.deim.asm.pract1_asm_alvaro_joan.ui.patinetes.model
 
+import cat.urv.deim.asm.pract1_asm_alvaro_joan.persistence.Scooter
 import java.util.*
 
 class Scooters{
-    var scooters: LinkedList<Scootera> = LinkedList()
+    var scooters: LinkedList<Scooter> = LinkedList()
 
 }
