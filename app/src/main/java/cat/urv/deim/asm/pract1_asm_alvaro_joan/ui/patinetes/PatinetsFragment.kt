3918321dfb3f46ad54ae87649cc562ee20c8ab3c
Alvaro2001B <100117640+Alvaro2001B@users.noreply.github.com>
2022-06-04 12:40:56 +0200

    Arreglos

--- app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/PatinetsFragment.kt
+++ app/src/main/java/cat/urv/deim/asm/pract1_asm_alvaro_joan/ui/patinetes/PatinetsFragment.kt
@@ -33,27 +33,27 @@ class PatinetsFragment : Fragment() {
     ): View {
         val slideshowViewModel =
             ViewModelProvider(this).get(PatinetsViewModel::class.java)
+
         _binding = FragmentPatinetsBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val textView: TextView = binding.textView
         slideshowViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-
-        return root
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         _binding = FragmentPatinetsBinding.inflate(layoutInflater)
         val view = binding.root
-        //val scooters: Scooters = ScooterRepository.activeScooters(requireContext(), AppConfig.DEFAULT_SCOOTER_RAW_JSON_FILE)
         val scooter:List<Scooter> = emptyList()
         val adapter: ScooterRecyclerViewAdapter = ScooterRecyclerViewAdapter(scooter)
         binding.scooterRecyclerView.adapter = adapter
         updateScooterRecycleView(adapter)
+        return root
+    }
 
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
     fun updateScooterRecycleView(adapter: ScooterRecyclerViewAdapter){
         GlobalScope.launch() {
